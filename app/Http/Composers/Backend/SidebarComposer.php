f7af745e3b01726f40a94f6d5819eae5161ddcf4
slavmin <slavmin@gmail.com>
2019-04-23 12:35:44 +0300

    styles fix

--- app/Http/Composers/Backend/SidebarComposer.php
+++ app/Http/Composers/Backend/SidebarComposer.php
@@ -32,11 +32,6 @@ class SidebarComposer
      */
     public function compose(View $view)
     {
-//        if (config('access.users.requires_approval')) {
-//            //$view->with('', $this->userRepository->getUnconfirmedCount());
-//            $view->with('pending_approval', $this->userRepository->getInactiveCount());
-//        } else {
-//            $view->with('pending_approval', 0);
-//        }
+
     }
 }
