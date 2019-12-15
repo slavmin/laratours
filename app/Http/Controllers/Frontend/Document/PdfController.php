e5311c237ef25828a1c25edd85c556c5cf7c5206
shoom1337 <kirillpavlik@mail.ru>
2019-12-15 17:48:39 +0300

    Pdf generator fully moved to php-controller.

--- app/Http/Controllers/Frontend/Document/PdfController.php
+++ app/Http/Controllers/Frontend/Document/PdfController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Frontend\Document;
 
+use App\Models\Document\Document;
+use App\Repositories\Frontend\Api\Document\LabelsRepository;
 use Illuminate\Http\Request;
 use Dompdf\Dompdf;
 
@@ -9,11 +11,38 @@ class PdfController extends BaseController
 {
     public function getPdf(Request $request) 
     {
-        $html_String = $request->string;
-        //dd($html_String, $request->all());
+        $document_id = $request->document_id;
+        $order_id = $request->order_id;
+        $tour_id = $request->tour_id;
+        $team_id = $request->team_id;
+
+        $document = Document::whereId($document_id)->AllTeams()->first();
+        
+        if ($request->team_mode) {
+            $order_info = 
+                (new LabelsRepository)
+                    ->getTeamLabelsWithValues($team_id);
+        }
+
+        if ($request->tourist_mode) {
+            $order_info = 
+                (new LabelsRepository)
+                    ->getTouristLabelsWithValues($order_id, $tour_id);
+        }
+
+        $labels = $order_info['labels'];
+
+        $start_html = "<html><head><title>Test html 2 pdf.</title><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"><style>@page { margin: 24px;}* { font-family: Arial, \"DejaVu Sans\", monospace;}</style><body>";
+        $end_html = "</body></html>";
+        $html_string = $start_html.$document->template.$end_html;
+        foreach ($labels as $key => $value) {
+            $replace_string = "{".$key."}";
+            $html_string = str_replace($replace_string, $value, $html_string);
+        }
+        // dd($html_string);
         $dompdf = new Dompdf();
         $dompdf->set_paper('A4');
-        $dompdf->loadHtml($html_String);
+        $dompdf->loadHtml($html_string);
         $dompdf->render();
         $dompdf->stream("dompdf.pdf");
     }
--- resources/js/frontend/components/documents/public/PDF.vue
+++ resources/js/frontend/components/documents/public/PDF.vue
@@ -1,12 +1,52 @@
 <template>
   <div>
     {{ document.name }}
-    <button @click="getPdf()">
-      <img 
-        src="/img/frontend/icons/pdf.png" 
-        width="32"
+    <form
+      action="/modules/get-pdf"
+      method="GET"
+    >
+      <input
+        name="document_id"
+        :value="document.id"
+        type="hidden"
       >
-    </button>
+      <div v-if="touristMode">
+        <input
+          name="tourist_mode"
+          :value="touristMode"
+          type="hidden"
+        >
+        <input 
+          name="order_id"
+          :value="orderId"
+          type="hidden"
+        >
+        <input 
+          name="tour_id"
+          :value="tourId"
+          type="hidden"
+        >
+      </div>
+      <div v-if="teamMode">
+        <input
+          name="team_mode"
+          :value="teamMode"
+          type="hidden"
+        >
+        <input
+          name="team_id"
+          :value="document.team_id"
+          type="hidden"
+        >
+      </div>
+        
+      <button type="submit">
+        <img 
+          src="/img/frontend/icons/pdf.png" 
+          width="32"
+        >
+      </button>
+    </form>
     <!-- <div
       id="document"
       v-html="document.template"
@@ -77,48 +117,48 @@ export default {
     console.log(this.document)
   },
   methods: {
-    getPdf() {
-      axios.get('/api/label-options', {
-        params: this.requestParameters,
-      })
-        .then(response => {
-          console.log(response)
-          this.labels = response.data[0].labels
-          console.log(this.labels)
-        })
-        .then(response => {
-          this.result = this.document.template
-          for (let key in this.labels) {
-            while (this.result.includes('{' + key + '}')) {
-              console.log(key)
-              this.result = this.result.replace('{' + key + '}', this.labels[key])
-            }
-            while (this.result.includes('\n')) {
-              this.result = this.result.replace('\n', '')
-            }
-            while (this.result.includes('\r')) {
-              this.result = this.result.replace('\r', '')
-            }
-            while (this.result.includes('"')) {
-              this.result = this.result.replace('"', '')
-            }
-          }
-          const startHtml = "<html><head><title>Test html 2 pdf.</title><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"><style>@page { margin: 24px;}* { font-family: Arial, \"DejaVu Sans\", monospace;}</style><body>"
-          const endHtml = "</body></html>"
-          let htmlString = `${startHtml}${this.result}${endHtml}`
-          console.log(htmlString)
-          window.location.replace(`/modules/get-pdf?string=${htmlString}`)
-        //  let opt = {
-        //    margin:       1,
-        //    pagebreak:    { mode: 'avoid-all', before: '#page2el' },
-        //    filename:     `${this.document.name}.pdf`,
-        //    image:        { type: 'jpeg', quality: 0.98 },
-        //    html2canvas:  { scale: 1, width: 900 },
-        //    jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
-        //  }
-        //  html2pdf().from(this.result).set(opt).save()
-        })
-    }
+    // getPdf() {
+    //   axios.get('/api/label-options', {
+    //     params: this.requestParameters,
+    //   })
+    //     .then(response => {
+    //       console.log(response)
+    //       this.labels = response.data[0].labels
+    //       console.log(this.labels)
+    //     })
+    //     .then(response => {
+    //       this.result = this.document.template
+    //       for (let key in this.labels) {
+    //         while (this.result.includes('{' + key + '}')) {
+    //           console.log(key)
+    //           this.result = this.result.replace('{' + key + '}', this.labels[key])
+    //         }
+    //         while (this.result.includes('\n')) {
+    //           this.result = this.result.replace('\n', '')
+    //         }
+    //         while (this.result.includes('\r')) {
+    //           this.result = this.result.replace('\r', '')
+    //         }
+    //         while (this.result.includes('"')) {
+    //           this.result = this.result.replace('"', '')
+    //         }
+    //       }
+    //       const startHtml = "<html><head><title>Test html 2 pdf.</title><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"><style>@page { margin: 24px;}* { font-family: Arial, \"DejaVu Sans\", monospace;}</style><body>"
+    //       const endHtml = "</body></html>"
+    //       let htmlString = `${startHtml}${this.result}${endHtml}`
+    //       console.log(htmlString)
+    //       window.location.replace(`/modules/get-pdf?string=${htmlString}`)
+    //     //  let opt = {
+    //     //    margin:       1,
+    //     //    pagebreak:    { mode: 'avoid-all', before: '#page2el' },
+    //     //    filename:     `${this.document.name}.pdf`,
+    //     //    image:        { type: 'jpeg', quality: 0.98 },
+    //     //    html2canvas:  { scale: 1, width: 900 },
+    //     //    jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
+    //     //  }
+    //     //  html2pdf().from(this.result).set(opt).save()
+    //     })
+    // }
   }
 }
 </script>
