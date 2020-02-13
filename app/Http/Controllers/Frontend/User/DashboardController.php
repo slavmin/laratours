2f5b75218307ca55e376048d0f978163def23a79
shoom1337 <kirillpavlik@mail.ru>
2020-02-13 18:16:17 +0300

    DashboardController. Fix role check. Admin had an error on login.

--- app/Http/Controllers/Frontend/User/DashboardController.php
+++ app/Http/Controllers/Frontend/User/DashboardController.php
@@ -11,27 +11,29 @@ use App\Models\Tour\TourHotelCategory;
  */
 class DashboardController extends Controller
 {
-    /**
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
-    public function index()
-    {
-        $team = Team::whereId(auth()->user()->current_team_id)->with('roles')->first();
+  /**
+   * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+   */
+  public function index()
+  {
+    $team = Team::whereId(auth()->user()->current_team_id)->with('roles')->first();
 
-        $show_fill_demo = false;
+    $show_fill_demo = false;
 
-        $isOperator = false;
-        $hasRole = $team->roles()->first();
+    $isOperator = false;
+    if ($team) {
+      $hasRole = $team->roles()->first();
 
-        if ($hasRole) {
-            $isOperator = ($team->roles->first()->name == 'operator') ? true : false;
-            $hotel_categories = TourHotelCategory::where('team_id', $team->id)->get();
-        }
-
-        if ($isOperator && count($hotel_categories) == 0) {
-            $show_fill_demo = true;
-        }
-
-        return view('frontend.user.dashboard', compact('team', 'show_fill_demo'));
+      if ($hasRole) {
+        $isOperator = ($team->roles->first()->name == 'operator') ? true : false;
+        $hotel_categories = TourHotelCategory::where('team_id', $team->id)->get();
+      }
     }
+
+    if ($isOperator && count($hotel_categories) == 0) {
+      $show_fill_demo = true;
+    }
+
+    return view('frontend.user.dashboard', compact('team', 'show_fill_demo'));
+  }
 }
