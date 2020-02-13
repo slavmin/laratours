8cbcac34e3f685bfd1cdbc7350204a87307e7c10
shoom1337 <kirillpavlik@mail.ru>
2020-02-13 18:40:46 +0300

    ForgotPassword form finished.

--- app/Http/Controllers/Frontend/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Frontend/Auth/ResetPasswordController.php
@@ -16,138 +16,139 @@ use App\Http\Requests\Frontend\Auth\ResetPasswordRequest;
  */
 class ResetPasswordController extends Controller
 {
-    use ResetsPasswords;
+  use ResetsPasswords;
 
-    /**
-     * @var UserRepository
-     */
-    protected $userRepository;
+  /**
+   * @var UserRepository
+   */
+  protected $userRepository;
 
-    /**
-     * ChangePasswordController constructor.
-     *
-     * @param UserRepository $userRepository
-     */
-    public function __construct(UserRepository $userRepository)
-    {
-        $this->userRepository = $userRepository;
+  /**
+   * ChangePasswordController constructor.
+   *
+   * @param UserRepository $userRepository
+   */
+  public function __construct(UserRepository $userRepository)
+  {
+    $this->userRepository = $userRepository;
+  }
+
+  /**
+   * Where to redirect users after login.
+   *
+   * @return string
+   */
+  public function redirectPath()
+  {
+    return 'frontend.auth.login';
+  }
+
+  /**
+   * Display the password reset view for the given token.
+   *
+   * If no token is present, display the link request form.
+   *
+   * @param string|null $token
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function showResetForm($token = null)
+  {
+    dd('hello');
+    if (!$token) {
+      return redirect()->route('frontend.auth.password.email');
     }
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @return string
-     */
-    public function redirectPath()
-    {
-        return 'frontend.auth.login';
+    $user = $this->userRepository->findByPasswordResetToken($token);
+
+    if ($user && resolve('auth.password.broker')->tokenExists($user, $token)) {
+      return view('frontend.auth.passwords.reset')
+        ->withToken($token)
+        ->withEmail($user->email);
     }
 
-    /**
-     * Display the password reset view for the given token.
-     *
-     * If no token is present, display the link request form.
-     *
-     * @param string|null $token
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function showResetForm($token = null)
-    {
-        if (! $token) {
-            return redirect()->route('frontend.auth.password.email');
-        }
+    return redirect()->route('frontend.auth.password.email')
+      ->withFlashDanger(__('exceptions.frontend.auth.password.reset_problem'));
+  }
 
-        $user = $this->userRepository->findByPasswordResetToken($token);
+  /**
+   * Reset the given user's password.
+   *
+   * @param  ResetPasswordRequest  $request
+   * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+   */
+  public function reset(ResetPasswordRequest $request)
+  {
+    // Here we will attempt to reset the user's password. If it is successful we
+    // will update the password on an actual user model and persist it to the
+    // database. Otherwise we will parse the error and return the response.
+    $response = $this->broker()->reset(
+      $this->credentials($request),
+      function ($user, $password) {
+        $this->resetPassword($user, $password);
+      }
+    );
 
-        if ($user && resolve('auth.password.broker')->tokenExists($user, $token)) {
-            return view('frontend.auth.passwords.reset')
-                ->withToken($token)
-                ->withEmail($user->email);
-        }
+    // If the password was successfully reset, we will redirect the user back to
+    // the application's home authenticated view. If there is an error we can
+    // redirect them back to where they came from with their error message.
+    return $response === Password::PASSWORD_RESET
+      ? $this->sendResetResponse($response)
+      : $this->sendResetFailedResponse($request, $response);
+  }
 
-        return redirect()->route('frontend.auth.password.email')
-            ->withFlashDanger(__('exceptions.frontend.auth.password.reset_problem'));
+
+  /**
+   * Reset the given user's password.
+   *
+   * @param $user
+   * @param $password
+   * @throws GeneralException
+   */
+  protected function resetPassword($user, $password)
+  {
+    // Check to see if the users account is confirmed and active
+    if (!$user->isConfirmed()) {
+      auth()->logout();
+
+      // If the user is pending (account approval is on)
+      if ($user->isPending()) {
+        throw new GeneralException(__('exceptions.frontend.auth.confirmation.pending'));
+      }
+
+      // Otherwise see if they want to resent the confirmation e-mail
+
+      throw new GeneralException(__('exceptions.frontend.auth.confirmation.resend', ['url' => route('frontend.auth.account.confirm.resend', e($user->{$user->getUuidName()}))]));
     }
 
-    /**
-     * Reset the given user's password.
-     *
-     * @param  ResetPasswordRequest  $request
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
-     */
-    public function reset(ResetPasswordRequest $request)
-    {
-        // Here we will attempt to reset the user's password. If it is successful we
-        // will update the password on an actual user model and persist it to the
-        // database. Otherwise we will parse the error and return the response.
-        $response = $this->broker()->reset(
-            $this->credentials($request),
-            function ($user, $password) {
-                $this->resetPassword($user, $password);
-            }
-        );
+    if (!$user->isActive()) {
+      auth()->logout();
 
-        // If the password was successfully reset, we will redirect the user back to
-        // the application's home authenticated view. If there is an error we can
-        // redirect them back to where they came from with their error message.
-        return $response === Password::PASSWORD_RESET
-            ? $this->sendResetResponse($response)
-            : $this->sendResetFailedResponse($request, $response);
+      throw new GeneralException(__('exceptions.frontend.auth.deactivated'));
     }
 
 
-    /**
-     * Reset the given user's password.
-     *
-     * @param $user
-     * @param $password
-     * @throws GeneralException
-     */
-    protected function resetPassword($user, $password)
-    {
-        // Check to see if the users account is confirmed and active
-        if (! $user->isConfirmed()) {
-            auth()->logout();
+    $user->password = $password;
 
-            // If the user is pending (account approval is on)
-            if ($user->isPending()) {
-                throw new GeneralException(__('exceptions.frontend.auth.confirmation.pending'));
-            }
+    $user->password_changed_at = now();
 
-            // Otherwise see if they want to resent the confirmation e-mail
+    $user->setRememberToken(Str::random(60));
 
-            throw new GeneralException(__('exceptions.frontend.auth.confirmation.resend', ['url' => route('frontend.auth.account.confirm.resend', e($user->{$user->getUuidName()}))]));
-        }
+    $user->save();
 
-        if (! $user->isActive()) {
-            auth()->logout();
+    event(new PasswordReset($user));
 
-            throw new GeneralException(__('exceptions.frontend.auth.deactivated'));
-        }
+    //$this->guard()->login($user);
+  }
 
-
-        $user->password = $password;
-
-        $user->password_changed_at = now();
-
-        $user->setRememberToken(Str::random(60));
-
-        $user->save();
-
-        event(new PasswordReset($user));
-
-        //$this->guard()->login($user);
-    }
-
-    /**
-     * Get the response for a successful password reset.
-     *
-     * @param  string  $response
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    protected function sendResetResponse($response)
-    {
-        return redirect()->route($this->redirectPath())->withFlashSuccess(e(trans($response)));
-    }
+  /**
+   * Get the response for a successful password reset.
+   *
+   * @param  string  $response
+   * @return \Illuminate\Http\RedirectResponse
+   */
+  protected function sendResetResponse($response)
+  {
+    return redirect()->route($this->redirectPath())->withFlashSuccess(e(trans($response)));
+  }
 }
--- resources/views/frontend/auth/passwords/email.blade.php
+++ resources/views/frontend/auth/passwords/email.blade.php
@@ -3,49 +3,56 @@
 @section('title', app_name() . ' | ' . __('labels.frontend.passwords.reset_password_box_title'))
 
 @section('content')
-    <div class="row justify-content-center align-items-center">
-        <div class="col col-sm-6 align-self-center">
-            <div class="card">
-                <div class="card-header">
-                    <strong>
-                        @lang('labels.frontend.passwords.reset_password_box_title')
-                    </strong>
-                </div><!--card-header-->
-
-                <div class="card-body">
-
-                    @if(session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ html()->form('POST', route('frontend.auth.password.email.post'))->open() }}
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    {{ html()->label(__('validation.attributes.frontend.email'))->for('email') }}
-
-                                    {{ html()->email('email')
-                                        ->class('form-control')
-                                        ->placeholder(__('validation.attributes.frontend.email'))
-                                        ->attribute('maxlength', 191)
-                                        ->required()
-                                        ->autofocus() }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
-
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group mb-0 clearfix">
-                                    {{ form_submit(__('labels.frontend.passwords.send_password_reset_link_button')) }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
-                    {{ html()->form()->close() }}
-                </div><!-- card-body -->
-            </div><!-- card -->
-        </div><!-- col-6 -->
-    </div><!-- row -->
+  <v-row justify="center">
+    <v-card outlined class="mt-5">
+      <v-toolbar
+        dark
+        color="#94CED7"
+      >
+        <v-card-title>
+          @lang('labels.frontend.passwords.reset_password_box_title')
+        </v-card-title>
+      </v-toolbar>
+      <v-list-item class="mt-5">
+        {{ html()->form('POST', route('frontend.auth.password.email.post'))->id('form')->open() }}
+        <v-text-field
+          name="email"
+          type="email"
+          label="{{ __('validation.attributes.frontend.email') }}"
+          value="{{ $item->name ?? '' }}"
+          maxlength="191"
+          outlined
+          required
+        ></v-text-field> 
+        {{ html()->form()->close() }}
+      </v-list-item>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn 
+          form="form"
+          type="submit"
+          class="ma-2" 
+          tile 
+          color="#aa282a" 
+          dark
+        >
+          {{ __('labels.frontend.passwords.send_password_reset_link_button') }}
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-row>
+  @if(session('status'))
+  <v-row justify="center">
+    <v-alert
+      dense
+      text
+      type="success"
+      class="mt-5"
+      border="left"
+      style="max-width: 350px;"
+    >
+    {{ session('status') }}
+    </v-alert>
+  </v-row>
+  @endif
 @endsection
