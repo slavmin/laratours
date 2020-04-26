523e449da52fbcc0caab432dbd51e1232cb672aa
shoom1337 <kirillpavlik@mail.ru>
2020-04-26 23:23:02 +0300

    Museums table. Min price fixed.

--- resources/views/frontend/tour/object/includes/museum/min-prices.blade.php
+++ resources/views/frontend/tour/object/includes/museum/min-prices.blade.php
@@ -3,11 +3,15 @@
     if (count($prices_array) > 0) {
         $min_price = $prices_array[0]->price; 
         foreach($prices_array as $price) {
-            if ($min_price > $price->price) {
+            if (($min_price > $price->price) && ($price->price != 0)) {
                 $min_price = $price->price;
             }
         }
-        $result = 'от ' . $min_price;
+        if ($min_price == 0) {
+            $result = 'бесплатно';
+        } else {
+            $result = 'от ' . $min_price;
+        }
     } else {
         $result = 'цены не найдены';
     }
